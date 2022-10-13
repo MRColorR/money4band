bdac2026ebb0e2843f09325a4198700aa159cbc3
MRColor <72873425+MRColorR@users.noreply.github.com>
2022-10-13 17:05:37 +0200

    updated bitping setup call code

--- bitpingSetup.ps1
+++ bitpingSetup.ps1
@@ -1,8 +1,8 @@
 #!/bin/pwsh
 echo "First of all make sure that you have already run the runme.ps1 script and that you have registered to the various apps with the links provided."
-Read-Host -prompt "if you have already performed these actions, press any key to continue"
+Read-Host -prompt "if you have already performed these actions, press enter to continue"
 echo "To configure bitping we will need to start an interactive container (so Docker needs to be already installed)."
 echo "Then when prompted enter your bitping email and password in it. Hit enter and then close it (or CTRL+C it) as we will not need it anymore"
-Read-Host -prompt "When ready to start, press any key to continue"
+Read-Host -prompt "When ready to start, press enter to continue"
 docker run --rm -it -v ${PWD}/.data/.bitping/:/root/.bitping bitping/bitping-node:latest
 echo "Bitping interactive container closed. Bitping config should be complete now."
