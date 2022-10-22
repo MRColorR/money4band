3232d88c381bc6e199a3afeb8662a007451a5be5
MRColor <mattiar11@gmail.com>
2022-10-22 17:44:27 +0200

    updated code for better longevity

--- install-docker.ps1
+++ install-docker.ps1
@@ -7,13 +7,13 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 
 function installDoker {
-    Write-Host "Downloading the Docker exe"
+    Write-Output "Downloading the Docker exe"
             Invoke-WebRequest -Uri https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe -OutFile DockerInstaller.exe -UseBasicParsing
-            Write-Host "Download Completed"
+            Write-Output "Download Completed"
 
-            Write-Host "Installing Docker please wait..."
+            Write-Output "Installing Docker please wait..."
             start-process .\DockerInstaller.exe -Wait -NoNewWindow -ArgumentList "install --accept-license --quiet"
-            Write-Host "Docker Installed successfully"
+            Write-Output "Docker Installed successfully"
             Read-Host "You must reboot the sytem to continue. After reboot re-run the script and proceed with the next steps"
             Restart-Computer -Confirm 
 }
@@ -24,22 +24,22 @@ $ProgressPreference = 'Continue'
 
     if($wsl.State -eq "Enabled") {
        
-        Write-Host "WSL is enabled."
+        Write-Output "WSL is enabled."
 
         if ([Environment]::Is64BitOperatingSystem)
         {
-            Write-Host "System is x64. Need to update Linux kernel..."
+            Write-Output "System is x64. Need to update Linux kernel..."
             if (-not (Test-Path wsl_update_x64.msi))
             {
-                Write-Host "Downloading Linux kernel update package..."
+                Write-Output "Downloading Linux kernel update package..."
                 Invoke-WebRequest -OutFile wsl_update_x64.msi https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi
             }
-            Write-Host "Installing Linux kernel update package..."
+            Write-Output "Installing Linux kernel update package..."
             Start-Process msiexec.exe -Wait -ArgumentList '/I wsl_update_x64.msi /quiet'
-            Write-Host "Linux kernel update package installed."
+            Write-Output "Linux kernel update package installed."
         }
 
-        Write-Host "WSL is enabled. Setting it to WSL2"
+        Write-Output "WSL is enabled. Setting it to WSL2"
         wsl --set-default-version 2
         
         if(Test-Path 'C:\Program Files\Docker\Docker\Docker Desktop.exe' -PathType Leaf){
@@ -47,11 +47,11 @@ $ProgressPreference = 'Continue'
             if ($yn -eq 'Y' -or $yn -eq 'y' -or $yn -eq 'Yes' -or $yn -eq 'yes' ){
                 installDoker
             } else {
-                Write-Host 'Docker installation/repair  canceled.'
-                Write-Host 'If docker was already correctly installed on your system you should be able to proceed with the next steps anyway'
+                Write-Output 'Docker installation/repair  canceled.'
+                Write-Output 'If docker was already correctly installed on your system you should be able to proceed with the next steps anyway'
             }
         } else {
-            Write-Host 'It seems that Docker is not installed on the system, the installation is starting'
+            Write-Output 'It seems that Docker is not installed on the system, the installation is starting'
             installDoker
         }
         
@@ -61,12 +61,12 @@ $ProgressPreference = 'Continue'
         
     } 
     else {
-        Write-Host "WSL is disabled."
-        Write-Host "Enabling WSL2 feature now"
+        Write-Output "WSL is disabled."
+        Write-Output "Enabling WSL2 feature now"
     
         & cmd /c 'dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart'
         & cmd /c 'dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart'
         Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
         Start-Sleep 30
-        Write-Host "WSL is enabled now reboot the system and re-run the script to continue the installation."
+        Write-Output "WSL is enabled now reboot the system and re-run the script to continue the installation."
     }
--- runme.ps1
+++ runme.ps1
@@ -20,51 +20,51 @@ $DKCOM_SRC = 'https://github.com/MRColorR/money4band/raw/main/docker-compose.yml
 $DKINST_WIN_SRC = 'https://github.com/MRColorR/money4band/raw/main/install-docker.ps1'
 
 ### Functions ##
-function fn_bye { echo "Bye bye."; exit 0; }
-function fn_fail { echo "Wrong option." exit 1; }
-function fn_unknown { echo "Unknown choice $REPLY, please choose a valid option"; }
+function fn_bye { Write-Output "Bye bye."; exit 0; }
+function fn_fail { Write-Output "Wrong option." exit 1; }
+function fn_unknown { Write-Output "Unknown choice $REPLY, please choose a valid option"; }
 
 ### Sub-menu Functions ##
 function fn_showLinks {
     clear;
-    echo "Use CTRL+Click to open links or copy them:"
-    echo $EARNAPP_LNK
-    echo $HONEYGAIN_LNK
-    echo $IPROYAL_LNK
-    echo $PACKETSTREAM_LNK
-    echo $PEER2PROFIT_LNK
-    echo $TRAFFMONETIZER_LNK
-    echo $BITPING_LNK
+    Write-Output "Use CTRL+Click to open links or copy them:"
+    Write-Output $EARNAPP_LNK
+    Write-Output $HONEYGAIN_LNK
+    Write-Output $IPROYAL_LNK
+    Write-Output $PACKETSTREAM_LNK
+    Write-Output $PEER2PROFIT_LNK
+    Write-Output $TRAFFMONETIZER_LNK
+    Write-Output $BITPING_LNK
     Read-Host -Prompt "Press enter to go back to mainmenu"
     mainmenu
 }
 
 function fn_dockerInstall {
     clear;
-    echo "This menu item will launch a script that will attempt to install docker"
-    echo "Use it only if you do not know how to perform the manual docker installation described at https://docs.docker.com/get-docker/ as the automatic script in some cases and depending on the OS you are using may fail to install docker correctly."
+    Write-Output "This menu item will launch a script that will attempt to install docker"
+    Write-Output "Use it only if you do not know how to perform the manual docker installation described at https://docs.docker.com/get-docker/ as the automatic script in some cases and depending on the OS you are using may fail to install docker correctly."
     $yn = Read-Host -Prompt "Do you wish to proceed with the Docker automatic installation Y/N?  "
     if ($yn -eq 'Y' -or $yn -eq 'y' -or $yn -eq 'Yes' -or $yn -eq 'yes' ) {
         clear;
-        echo "Which version of Docker do you want to install?"
-        echo "1) Install Docker for Linux"
-        echo "2) Install Docker for Windows"
+        Write-Output "Which version of Docker do you want to install?"
+        Write-Output "1) Install Docker for Linux"
+        Write-Output "2) Install Docker for Windows"
         $yn = Read-Host
         Switch ($Select) {
             1 {
-                echo "Starting Docker for linux auto installation script"
-                curl  https://get.docker.com -o get-docker.sh;
+                Write-Output "Starting Docker for linux auto installation script"
+                curl  -o 'get-docker.sh' https://get.docker.com ;
                 sudo sh get-docker.sh;
-                echo "Script completed. Docker should be installed"
+                Write-Output "Script completed. Docker should be installed"
                 Read-Host -Prompt "Press enter to go back to mainmenu"
                 mainmenu
             }
             2 {
-                echo "Starting Docker for Windows auto installation script"
+                Write-Output "Starting Docker for Windows auto installation script"
                 Invoke-WebRequest $DKINST_WIN_SRC -o install-docker.ps1
                 Start-Process PowerShell -Verb RunAs "-noprofile -executionpolicy bypass -Command `"cd '$pwd'; & '.\install-docker.ps1';`"" -Wait
                 
-                echo "Script completed. Docker should be installed. Please restart your computer and the proceed to .env file config and stack startup."
+                Write-Output "Script completed. Docker should be installed. Please restart your computer and the proceed to .env file config and stack startup."
                 Read-Host -Prompt "Press enter to go back to mainmenu"
                 mainmenu
             }
@@ -75,7 +75,7 @@ function fn_dockerInstall {
     }
     else {
         clear;
-        echo "Docker unattended installation canceled. Make sure you have docker installed before proceeding with the other steps.  ";
+        Write-Output "Docker unattended installation canceled. Make sure you have docker installed before proceeding with the other steps.  ";
         Read-Host -prompt "Press enter to go back to the menu";
         mainmenu;
     }
@@ -86,29 +86,29 @@ function fn_setupEnv {
     $yn = Read-Host -p "Do you wish to proceed with the .env file guided setup Y/N?  "
     if ($yn -eq 'Y' -or $yn -eq 'y' -or $yn -eq 'Yes' -or $yn -eq 'yes' ) {
         clear;
-        echo "Beginnning env file guided setup"
+        Write-Output "Beginnning env file guided setup"
         #touch .env
         $DEVICE_NAME = Read-Host -prompt "PLEASE ENTER A NAME FOR YOUR DEVICE:"
         (Get-Content .\.env).replace('yourDeviceName', "$DEVICE_NAME") | Set-Content .\.env
 
-        echo "PLEASE REGISTER ON THE PLATFORMS USING THIS LINKS, YOU'LL NEED TO ENTER SOME DATA BELOW:"
-        echo "Use CTRL+Click to open links or copy them:"
+        Write-Output "PLEASE REGISTER ON THE PLATFORMS USING THIS LINKS, YOU'LL NEED TO ENTER SOME DATA BELOW:"
+        Write-Output "Use CTRL+Click to open links or copy them:"
 
         #EarnApp app env setup
         
-        echo "Go to $EARNAPP_LNK and register"
+        Write-Output "Go to $EARNAPP_LNK and register"
         Read-Host -prompt "When done, press enter to continue"
-        echo "generating an UUID for earnapp"
+        Write-Output "generating an UUID for earnapp"
         $md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
         $utf8 = New-Object -TypeName System.Text.UTF8Encoding
         $UUID = [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($DEVICE_NAME))).replace("-", "").ToLower()
         (Get-Content .\.env).replace('yourMD5sum', "$UUID") | Set-Content .\.env
-        echo "!!SAVE THE FOLLOWING LINK SOMEWHERE TO CLAIM YOUR EARNAPP NODE after completing the setup and after starting the apps stack: https://earnapp.com/r/sdk-node-$UUID"
+        Write-Output "!!SAVE THE FOLLOWING LINK SOMEWHERE TO CLAIM YOUR EARNAPP NODE after completing the setup and after starting the apps stack: https://earnapp.com/r/sdk-node-$UUID"
         Read-Host -prompt "When done, press enter to continue to the next app"
 
         #HoneyGain app env setup
         clear
-        echo "Go to $HONEYGAIN_LNK and register"
+        Write-Output "Go to $HONEYGAIN_LNK and register"
         Read-Host -prompt "When done, press enter to continue"
         $HG_EMAIL = Read-Host -prompt "Enter your HoneyGain Email"
         (Get-Content .\.env).replace('yourHGMail', "$HG_EMAIL") | Set-Content .\.env
@@ -117,7 +117,7 @@ function fn_setupEnv {
 
         #Pawn IPRoyal app env setup
         clear
-        echo "Go to $IPROYAL_LNK and register"
+        Write-Output "Go to $IPROYAL_LNK and register"
         Read-Host -prompt "When done, press enter to continue"
         $IR_EMAIL = Read-Host -prompt "Enter your Pawn IPRoyal Email"
         (Get-Content .\.env).replace('yourIRMail', "$IR_EMAIL") | Set-Content .\.env
@@ -126,47 +126,47 @@ function fn_setupEnv {
 
         #Peer2Profit app env setup
         clear
-        echo "Go to $PEER2PROFIT_LNK and register"
+        Write-Output "Go to $PEER2PROFIT_LNK and register"
         Read-Host -prompt "When done, press enter to continue"
         $P2P_EMAIL = Read-Host -prompt "Enter your Peer2Profit Email"
         (Get-Content .\.env).replace('yourP2PMail', "$P2P_EMAIL") | Set-Content .\.env
 
         #PacketStream app env setup
         clear
-        echo "Go to $PACKETSTREAM_LNK and register"
+        Write-Output "Go to $PACKETSTREAM_LNK and register"
         Read-Host -prompt "When done, press enter to continue"
-        echo "Enter your PacketStream CID."
-        echo "You can find it going in your dashboard https://packetstream.io/dashboard/download?linux# then click on -> Looking for linux app -> now search for CID= in the code shown in the page (you can also use CTRL+F) you need to enter the code after -e CID= (e.g. if in the code CID=6aTk, just enter 6aTk)"
+        Write-Output "Enter your PacketStream CID."
+        Write-Output "You can find it going in your dashboard https://packetstream.io/dashboard/download?linux# then click on -> Looking for linux app -> now search for CID= in the code shown in the page (you can also use CTRL+F) you need to enter the code after -e CID= (e.g. if in the code CID=6aTk, just enter 6aTk)"
         $PS_CID = Read-Host PS_CID
         (Get-Content .\.env).replace('yourPSCID', "$PS_CID") | Set-Content .\.env
 
         # TraffMonetizer app env setup
         clear
-        echo "Go to $TRAFFMONETIZER_LNK and register"
+        Write-Output "Go to $TRAFFMONETIZER_LNK and register"
         Read-Host -prompt "When done, press enter to continue"
-        echo "Enter your TraffMonetizer Token."
-        echo "You can find it going in your dashboard https://app.traffmonetizer.com/dashboard then -> Look for Your application token -> just insert it here (you can also copy and then paste it)"
+        Write-Output "Enter your TraffMonetizer Token."
+        Write-Output "You can find it going in your dashboard https://app.traffmonetizer.com/dashboard then -> Look for Your application token -> just insert it here (you can also copy and then paste it)"
         $TM_TOKEN = Read-Host TM_TOKEN
         (Get-Content .\.env).replace('yourTMToken', "$TM_TOKEN") | Set-Content .\.env
     
         # Bitping app env setup
         clear
-        echo "Go to $BITPING_LNK and register"
+        Write-Output "Go to $BITPING_LNK and register"
         Read-Host -prompt "When done, press enter to continue"
-        echo "To configure this app we will need to start an interactive container (so Docker needs to be already installed)."
-        echo "Then wait and enter your bitping email and password in it when prompted, hit enter and then close it as we will not need it anymore"
-        echo "To do that we will open a new terminal in this same folder and run bitpingSetup.sh for you"
+        Write-Output "To configure this app we will need to start an interactive container (so Docker needs to be already installed)."
+        Write-Output "Then wait and enter your bitping email and password in it when prompted, hit enter and then close it as we will not need it anymore"
+        Write-Output "To do that we will open a new terminal in this same folder and run bitpingSetup.sh for you"
         Read-Host -prompt "When ready to start, press enter to continue"
         Start-Process PowerShell -Verb RunAs "-noprofile -executionpolicy bypass -Command `"cd '$pwd'; & '.\bitpingSetup.ps1';`""
 
-        echo "env file setup complete."
+        Write-Output "env file setup complete."
         Read-Host -prompt "Press enter to go back to the menu";
 
         mainmenu;
 
     }
     else {
-        echo ".env file setup canceled. Make sure you have a valid .env file before proceeding with the stack startup.";
+        Write-Output ".env file setup canceled. Make sure you have a valid .env file before proceeding with the stack startup.";
         Read-Host -prompt "Press enter to go back to the menu";
         mainmenu;
     }
@@ -174,41 +174,45 @@ function fn_setupEnv {
 }
 
 function fn_startStack {
-    echo "This menu item will launch all the apps using the configured .env file and the docker-compose.yml file (Docker must be already installed and running)"
+    Write-Output "This menu item will launch all the apps using the configured .env file and the docker-compose.yml file (Docker must be already installed and running)"
     $yn = Read-Host -prompt "Do you wish to proceed Y/N?"
     if ($yn -eq 'Y' -or $yn -eq 'y' -or $yn -eq 'Yes' -or $yn -eq 'yes' ) {
         docker compose up -d
-        echo "All Apps started. If not already done use the previously generated earnapp node URL to add your device in your earnapp dashboard. Check the README file for more details.";
+        Write-Output "All Apps started. If not already done use the previously generated earnapp node URL to add your device in your earnapp dashboard. Check the README file for more details.";
         mainmenu;
     }
     else {
-        echo "Docker unattended installation canceled. Make sure you have docker installed before proceeding with the other steps.  "
+        Write-Output "Docker unattended installation canceled. Make sure you have docker installed before proceeding with the other steps.  "
         Read-Host -prompt "Press enter to go back to the menu";
         mainmenu;
     }
 }
 
 function fn_resetEnv {
-    echo "Now a fresh env file will be downloaded and will need to be reconfigured to be used again"
+    Write-Output "Now a fresh env file will be downloaded and will need to be reconfigured to be used again"
     $yn = Read-Host -prompt "Do you wish to proceed Y/N?  "
     if ($yn -eq 'Y' -or $yn -eq 'y' -or $yn -eq 'Yes' -or $yn -eq 'yes' ) {
-        curl -LJO $ENV_SRC; echo ".env file resetted, remember to reconfigure it";
+        curl -o '.env' $ENV_SRC; Write-Output ".env file resetted, remember to reconfigure it";
+        Read-Host -prompt "Press enter to go back to the menu";
+        mainmenu;
     }
     else {
-        echo ".env file reset canceled. The file is left as it is.  "
+        Write-Output ".env file reset canceled. The file is left as it is.  "
         Read-Host -prompt "Press enter to go back to the menu";
         mainmenu;
     }
 }
 
 function fn_resetDockerCompose{
-    echo "Now a fresh docker-compose.yml file will be downloaded"
+    Write-Output "Now a fresh docker-compose.yml file will be downloaded"
     $yn = Read-Host -prompt "Do you wish to proceed Y/N?  "
     if ($yn -eq 'Y' -or $yn -eq 'y' -or $yn -eq 'Yes' -or $yn -eq 'yes' ) {
-        curl -LJO $DKCOM_SRC; echo "docker-compose.yml file resetted, remember to reconfigure it if needed";
+        curl -o docker-compose.yml $DKCOM_SRC; Write-Output "docker-compose.yml file resetted, remember to reconfigure it if needed";
+        Read-Host -prompt "Press enter to go back to the menu";
+        mainmenu;
     }
     else {
-        echo "docker-compose.yml file reset canceled. The file is left as it is. "
+        Write-Output "docker-compose.yml file reset canceled. The file is left as it is. "
         Read-Host -prompt "Press enter to go back to the menu";
         mainmenu;
     }
