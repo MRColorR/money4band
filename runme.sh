c3ccff55c0ad40b7b940b2b5a22b937a9ec8470f
MRColor <mattiar11@gmail.com>
2022-10-24 14:33:02 +0200

    smarter env file config script

--- runme.sh
+++ runme.sh
@@ -17,17 +17,17 @@ cyanprint() { printf "${CYAN}%s${RESET}\n" "$1"; }
 ### Links ##
 readonly EARNAPP_LNK="Earnapp | https://earnapp.com/i/3zulx7k"
 readonly HONEYGAIN_LNK="HoneyGain | https://r.honeygain.me/MINDL15721"
-readonly IPROYAL_LNK="IPROYAL | https://pawns.app?r=MiNe"
+readonly IPROYALPAWNS_LNK="IPROYAL | https://pawns.app?r=MiNe"
 readonly PACKETSTREAM_LNK="PACKETSTREAM | https://packetstream.io/?psr=3zSD"
 readonly PEER2PROFIT_LNK="PEER2PROFIT | https://p2pr.me/165849012262da8d0aa13c8"
 readonly TRAFFMONETIZER_LNK="TRAFFMONETIZER | https://traffmonetizer.com/?aff=366499"
 readonly BITPING_LNK="BITPING | https://app.bitping.com?r=qm7mIuX3"
 
 ### .env File Prototype Link##
-readonly ENV_SRC='https://github.com/MRColorR/money4band/raw/main/.env'
+readonly ENV_SRC='https://github.com/MRColorR/money4band/raw/main/.env';
 
 ### docker-compose.yml Prototype Link##
-readonly $DKCOM_SRC = 'https://github.com/MRColorR/money4band/raw/main/docker-compose.yml'
+readonly DKCOM_SRC='https://github.com/MRColorR/money4band/raw/main/docker-compose.yml';
 
 ### Functions ##
 fn_bye() { echo "Bye bye."; exit 0; }
@@ -40,7 +40,7 @@ fn_showLinks(){
     greenprint "Use CTRL+Click to open links or copy them:";
                 cyanprint "1) $EARNAPP_LNK";
                 cyanprint "2) $HONEYGAIN_LNK";
-                cyanprint "3) $IPROYAL_LNK";
+                cyanprint "3) $IPROYALPAWNS_LNK";
                 cyanprint "4) $PACKETSTREAM_LNK";
                 cyanprint "5) $PEER2PROFIT_LNK";
                 cyanprint "6) $TRAFFMONETIZER_LNK";
@@ -76,6 +76,33 @@ fn_setupNotifications(){
     clear;
 }
 
+fn_setupApp(){
+    if [ "$2" == "email" ] ; then 
+        read APP_EMAIL -p "Enter your $1 Email"$'\n'
+        sed -i "s/your$1Mail/$APP_EMAIL/" .env
+        if [ "$3" == "password" ] ; then 
+        read APP_PASSWORD -p "Now enter your $1 Password"$'\n'
+        sed -i "s/your$1Pw/$APP_PASSWORD/" .env
+
+    elif [ "$2" == "uuid" ] ; then
+        echo "generating an UUID for $1"$'\n'
+        UUID="$(echo -n "$3" | md5sum | cut -c1-32)"
+        sed -i "s/yourMD5sum/$UUID/" .env
+        cyanprint "Save the following link somewhere to claim your earnapp node after completing the setup and after starting the apps stack: https://earnapp.com/r/sdk-node-$UUID. A new file containing this link has been created for you"
+        printf "https://earnapp.com/r/sdk-node-$UUID" > ClaimEarnappNode.txt
+    elif [ "$2" == "cid" ] ; then 
+        echo "Enter your $1 CID."$'\n'
+        echo "You can find it going in your dashboard https://packetstream.io/dashboard/download?linux# then click on -> Looking for linux app -> now search for CID= in the code shown in the page (you can also use CTRL+F) you need to enter the code after -e CID= (e.g. if in the code CID=6aTk, just enter 6aTk)"$'\n'
+        read APP_CID
+        sed -i "s/your$1CID/$APP_CID/" .env
+    elif [ "$2" == "token" ] ; then 
+        echo "Enter your $1 Token."$'\n'
+        echo "You can find it going in your dashboard https://app.traffmonetizer.com/dashboard then -> Look for Your application token -> just insert it here (you can also copy and then paste it)"$'\n'
+        read APP_TOKEN
+        sed -i "s/your$1Token/$APP_TOKEN/" .env
+    fi
+}
+
 fn_setupEnv(){
     read -p "Do you wish to proceed with the .env file guided setup Y/N?  " yn
     case $yn in
@@ -84,8 +111,8 @@ fn_setupEnv(){
         * ) echo "Please answer yes or no.";;
     esac
     echo "beginnning env file guided setup"
-    read -n 1 -s -r -p "Press any key to continue"$'\n'
-    touch .env
+    CURRENT_APP='';
+    read -p "Press enter to continue"$'\n'
     yellowprint "PLEASE ENTER A NAME FOR YOUR DEVICE:"
     read DEVICE_NAME
     sed -i "s/yourDeviceName/$DEVICE_NAME/" .env
@@ -94,66 +121,58 @@ fn_setupEnv(){
     greenprint "Use CTRL+Click to open links or copy them:"
 
     #EarnApp app env setup
-    cyanprint "Go to $EARNAPP_LNK and register"
-    read -n 1 -s -r -p "When done, press any key to continue"$'\n'
-    echo "generating an UUID for earnapp"$'\n'
-    UUID="$(echo -n "$DEVICE_NAME" | md5sum | cut -c1-32)"
-    sed -i "s/yourMD5sum/$UUID/" .env
-    cyanprint "Save the following link somewhere to claim your earnapp node after completing the setup and after starting the apps stack: https://earnapp.com/r/sdk-node-$UUID"
-    read -p "When done, press enter to continue to the next app"
+    CURRENT_APP='EARNAPP';
+    cyanprint "Go to $EARNAPP_LNK and register";
+    read -p "When done, press enter to continue"$'\n'
+    fn_setupApp $CURRENT_APP uuid $DEVICE_NAME;
+    read -p "$CURRENT_APP configuration complete, press enter to continue to the next app"
 
     #HoneyGain app env setup
     clear;
+    CURRENT_APP='HONEYGAIN';
     cyanprint "Go to $HONEYGAIN_LNK and register"
-    read -n 1 -s -r -p "When done, press any key to continue"$'\n'
-    echo "Enter your HoneyGain Email"$'\n'
-    read HG_EMAIL
-    sed -i "s/yourHGMail/$HG_EMAIL/" .env
-    echo "Now enter your HoneyGain Password"$'\n'
-    read HG_PASSWORD
-    sed -i "s/yourHGPw/$HG_PASSWORD/" .env
+    read -p "When done, press enter to continue"$'\n'
+    fn_setupApp $CURRENT_APP email password
+    read -p "$CURRENT_APP configuration complete, press enter to continue to the next app"
 
     #Pawn IPRoyal app env setup
     clear;
-    cyanprint "Go to $IPROYAL_LNK and register"
-    read -n 1 -s -r -p "When done, press any key to continue"$'\n'
-    echo "Enter your Pawn IPRoyal Email"$'\n'
-    read IR_EMAIL
-    sed -i "s/yourIRMail/$IR_EMAIL/" .env
-    echo "Now enter your IPRoyal Password"$'\n'
-    read IR_PASSWORD
-    sed -i "s/yourIRPw/$IR_PASSWORD/" .env
+    CURRENT_APP='IPROYALPAWNS'
+    cyanprint "Go to $IPROYALPAWNS_LNK and register"
+    read -p "When done, press enter to continue"$'\n'
+    fn_setupApp $CURRENT_APP email password
+    read -p "$CURRENT_APP configuration complete, press enter to continue to the next app"
 
     #Peer2Profit app env setup
     clear;
+    CURRENT_APP='PEER2PROFIT'
     cyanprint "Go to $PEER2PROFIT_LNK and register"
-    read -n 1 -s -r -p "When done, press any key to continue"$'\n'
-    echo "Enter your Peer2Profit Email"$'\n'
-    read P2P_EMAIL
-    sed -i "s/yourP2PMail/$P2P_EMAIL/" .env
+    read -p "When done, press enter to continue"$'\n'
+    fn_setupApp $CURRENT_APP email
+    read -p "$CURRENT_APP configuration complete, press enter to continue to the next app"
 
     #PacketStream app env setup
     clear;
+    CURRENT_APP='PACKETSTREAM'
     cyanprint "Go to $PACKETSTREAM_LNK and register"
-    read -n 1 -s -r -p "When done, press any key to continue"$'\n'
-    echo "Enter your PacketStream CID."$'\n'
-    echo "You can find it going in your dashboard https://packetstream.io/dashboard/download?linux# then click on -> Looking for linux app -> now search for CID= in the code shown in the page (you can also use CTRL+F) you need to enter the code after -e CID= (e.g. if in the code CID=6aTk, just enter 6aTk)"$'\n'
-    read PS_CID
-    sed -i "s/yourPSCID/$PS_CID/" .env
+    read -p "When done, press enter to continue"$'\n'
+    fn_setupApp $CURRENT_APP cid
+    read -p "$CURRENT_APP configuration complete, press enter to continue to the next app"
+
 
     # TraffMonetizer app env setup
     clear;
+    CURRENT_APP='TRAFFMONETIZER'
     cyanprint "Go to $TRAFFMONETIZER_LNK and register"
-    read -n 1 -s -r -p "When done, press any key to continue"$'\n'
-    echo "Enter your TraffMonetizer Token."$'\n'
-    echo "You can find it going in your dashboard https://app.traffmonetizer.com/dashboard then -> Look for Your application token -> just insert it here (you can also copy and then paste it)"$'\n'
-    read TM_TOKEN
-    sed -i "s/yourTMToken/$TM_TOKEN/" .env
+    read -p "When done, press enter to continue"$'\n'
+    fn_setupApp $CURRENT_APP token
+    read -p "$CURRENT_APP configuration complete, press enter to continue to the next app"
+
     
     # Bitping app env setup
     clear;
     cyanprint "Go to $BITPING_LNK and register"
-    read -n 1 -s -r -p "When done, press any key to continue"$'\n'
+    read -p "When done, press enter to continue"$'\n'
     echo "To configure this app we will need to start an interactive container (so Docker needs to be already installed)."
     echo "To do that we will open a new terminal in this same folder and run bitpingSetup.sh for you"$'\n'
     read -n 1 -s -r -p "When ready to start, press any key to continue"$'\n'
