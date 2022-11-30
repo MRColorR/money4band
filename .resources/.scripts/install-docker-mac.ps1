297ece4dbd8d2be1a07047878dad1f7c1303815a
MRColor <mattiar11@gmail.com>
2022-11-30 16:23:49 +0100

    adding docker for mac install support

--- .resources/.scripts/install-docker-mac.ps1
+++ .resources/.scripts/install-docker-mac.ps1
@@ -3,15 +3,16 @@ param(
     [string(Mandatory)]$filePath,
     [switch]$IntelCPU
     )
-
+$cpu = ""
 if ($IntelCPU) {
-    Write-Output "Selected Intel CPU"
+    $cpu = "Intel"
     #Invoke-WebRequest https://desktop.docker.com/mac/main/amd64/Docker.dmg -o "$filePath/Docker.dmg";
 }else {
-    Write-Output "Selected Appla silicon arm CPU"
+    $cpu = "Apple silicon arm"
     #Invoke-WebRequest https://desktop.docker.com/mac/main/arm64/Docker.dmg -o "$filePath/Docker.dmg";
 }
-sudo hdiutil attach "$filePath"
+Read-Host -Prompt "Ready. Press enter to install Docker for Mac with $cpu CPU"
+sudo hdiutil attach "$filePath/Docker.dmg"
 sudo /Volumes/Docker/Docker.app/Contents/MacOS/install --accept-license
 sudo hdiutil detach /Volumes/Docker
 open /Applications/Docker.app
