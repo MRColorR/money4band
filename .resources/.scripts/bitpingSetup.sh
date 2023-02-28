a95ff3edd2e89f027db602573d01fa4a22c7946c
MRColor <mattiar11@gmail.com>
2023-02-28 14:09:53 +0100

    fixed bitping setup script using LF

--- .resources/.scripts/bitpingSetup.sh
+++ .resources/.scripts/bitpingSetup.sh
@@ -4,7 +4,5 @@ read -n 1 -s -r -p "if you have already performed these actions, press any key t
 echo "To configure this app we will need to start an interactive container in a new terminal (so Docker needs to be already installed)."
 echo "Then when prompted enter your bitping email and password in it. Hit enter and then close it (or CTRL+C it) as we will not need it anymore"$'\n'
 read -n 1 -s -r -p "When ready to start, press any key to continue"$'\n'
-BITPING_DATA_PATH="${PWD}/.data/.bitping/"
-echo $BITPING_DATA_PATH
-sudo docker run --rm -it -v "$BITPING_DATA_PATH":/root/.bitping bitping/bitping-node:latest
+sudo docker run --rm -it -v "${PWD}"/.data/.bitping/:/root/.bitping bitping/bitping-node:latest && echo "ok"
 echo "Bitping interactive container closed. Bitping config should be complete now."
