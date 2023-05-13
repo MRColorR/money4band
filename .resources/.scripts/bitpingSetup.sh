8b15795a74f38976f1c051cfd73761069caafc5f
MRColor <mattiar11@gmail.com>
2023-05-13 13:11:21 +0200

    simpler messages in the bitping setup script as it should always be launched by the runme.sh

--- .resources/.scripts/bitpingSetup.sh
+++ .resources/.scripts/bitpingSetup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo "First of all make sure that you have already run the runme.sh script and that you have registered to the various apps with the links provided."
-read -n 1 -s -r -p "if you have already performed these actions, press any key to continue"$'\n'
 echo "To configure this app we will need to start an interactive container in a new terminal (so Docker needs to be already installed)."
 echo "Then when prompted enter your bitping email and password in it. Hit enter and then close it (or CTRL+C it) as we will not need it anymore"$'\n'
 read -n 1 -s -r -p "When ready to start, press any key to continue"$'\n'
