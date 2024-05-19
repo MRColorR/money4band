2915abcc2c48a874480f3675d5d063638d4583a5
Sam2029 <119959793+Carbon2029@users.noreply.github.com>
2024-05-19 09:06:04 +0530

    Got the front end of setup apps working Changed iprooyalpawns to pawns in config files
    Things left to do
    setup advanced mode
    write the final config to file
    rest confg file script

--- utils/fn_setupApps.py
+++ utils/fn_setupApps.py
@@ -13,12 +13,42 @@ def write_json():
     Used to write to user-config.json
     can be moved to load but I am not sure soooo yeah let it here for now
     '''
-def main(user_config:dict,m4b_config:dict):
+def main(app_config:dict,m4b_config:dict):
+    user_config = load.load_json_config('./config/user-config.json')
     advance_setup = input('Do you want to go with advanced setup?(y/n)')
     if advance_setup.lower().strip(' ') == 'y':
         "For the pros who prefer quick setup"
         pass
     else:
         "The same previous setup method"
+
+        nickename = input('Enter your nickname')
+        #Dont think email is needed
+        device_name = input('Enter your device name')
+
+        user_config['user']['Nickname'] = nickename
+        user_config['device_info']['device_name'] = device_name
+
+
+        #ask proxy info lazy to code rn
+
+        #Set up apps now
+        asking = {'enabled':'Do you want to run {}','email':'Enter your {} email','password':'Enter you {} password','apikey':'Enter your {} api key','cid':'Enter your {} cid','token':'Enter your {} token','code':'Enter your {} code'}
+
+        for app in user_config['apps']:
+            cls()
+            for property in user_config['apps'][app]:
+                if property in asking:
+                    user_input = input(asking[property].format(app.title()))
+                    print(user_input.lower().strip(' '))
+                    if property == 'enabled' and user_input.lower().strip(' ') !='y':
+                        print('skipping app')
+                        break
+                    
+
+
+
+
+
 
 
