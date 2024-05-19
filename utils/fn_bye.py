5ae9f9f45c02d64ea2552150ab4faab888bae4fa
Sam2029 <119959793+Carbon2029@users.noreply.github.com>
2024-05-19 22:15:03 +0530

    Set up a quick fn_bye script
    started working on running apps

---
+++ utils/fn_bye.py
@@ -0,0 +1,15 @@
+import os
+import argparse
+import logging
+import locale
+import time
+from typing import Dict, Any
+from colorama import Fore, Back, Style, just_fix_windows_console
+from utils import load, detect
+from utils.cls import cls
+import random
+
+def main(app_config:dict,m4b_config:dict):
+    cls()
+    print('Qutting the app')
+    print(random.choice(['Have a great day........','If you see this then sam probably suceeded making this yay!!!','Look at the eyes deeper they never lie!!','Enjoy the rest of your day','The world will be never with you when you need it the most :(']))
