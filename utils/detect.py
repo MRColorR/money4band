40e30e60bd5cd76295bb25ba741d3b993997247d
MRColorR <mattiar11@gmail.com>
2024-05-20 12:07:42 +0200

    FIX - detect wrong variables init and logging alignment

--- utils/detect.py
+++ utils/detect.py
@@ -59,13 +59,6 @@ def main(m4b_config_path: str) -> None:
     m4b_config_path -- the path to the m4b config file
     """
     try:
-        # Start logging
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        script_name = os.path.basename(__file__)
-        log_dir = os.path.join(script_dir, 'logs')
-        os.makedirs(log_dir, exist_ok=True)
-        logging.basicConfig(filename=os.path.join(log_dir, f"{script_name}.log"), format='%(asctime)s - [%(levelname)s] - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
-
         # Test the function
         msg = f"Testing {script_name} function"
         print(msg)
@@ -82,10 +75,20 @@ def main(m4b_config_path: str) -> None:
         raise
 
 if __name__ == "__main__":
+    # Get the script absolute path and name
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    script_name = os.path.basename(__file__)
+
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description=f"Run the {script_name} module standalone.")
     parser.add_argument('--m4b-config-path', type=str, required=True, help='The m4b config file path')
+    parser.add_argument('--log-dir', default=os.path.join(script_dir, 'logs'), help='Set the logging directory')
+    parser.add_argument('--log-file', default=f"{script_name}.log", help='Set the logging file name')
     args = parser.parse_args()
+
+    # Start logging
+    os.makedirs(args.log_dir, exist_ok=True)
+    logging.basicConfig(filename=os.path.join(args.log_dir, args.log_file),  format='%(asctime)s - [%(levelname)s] - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level="DEBUG")
 
     # Call the main function
     main(args.m4b_config_path)
