f802eff65fcc57c40b4d74be11792b216981548c
MRColor <72873425+MRColorR@users.noreply.github.com>
2024-05-29 17:02:07 +0200

    WIP

--- utils/fn_startStack.py
+++ utils/fn_startStack.py
@@ -3,41 +3,45 @@ import argparse
 import logging
 import locale
 import time
+import subprocess
 from typing import Dict, Any
 from colorama import Fore, Back, Style, just_fix_windows_console
 from utils import loader, detector
 from utils.cls import cls
 import json
-import subprocess
 
-def main(app_config:dict,m4b_config:dict):
+def main(app_config: Dict[str, Any], m4b_config: Dict[str, Any]) -> None:
     user_config = loader.load_json_config('./config/user-config.json')
     for app in app_config['apps']:
+        app_name = app.get('name', '').lower()
 
-        app_name = app['name'].lower()
-
-        if user_config['apps'][app_name]['enabled']:
+        if user_config['apps'].get(app_name, {}).get('enabled', False):
+            logging.info(f'Starting setup for enabled {app_name.title()} app')
+            app_image = app.get('image')
+            if app_image is None:
+                logging.error(f'No image for {app_name}')
+                continue
+            
+            app_flags = app.get('flags', {})
+            app_additional_args = app.get('additional_args', {})
 
             print(f'Pulling {app_name.title()} container')
-            subprocess.run(f'docker pull {app['image']}')
+            subprocess.run(['docker', 'pull', app_image])
             time.sleep(m4b_config['system']['sleep_time'])
 
-            #now run the app with appropriate args
-            extra_global = {'device_name':'device_info'}
-            run_command = ['docker','run','-d','--name',app_name]
+            # Now run the app with appropriate args
+            extra_global = {'device_name': 'device_info'}
+            run_command = ['docker', 'run', '-d', '--name', app_name, app_image]
 
-            run_command.append(app['image'])
-            for i in app['flags']:
-                run_command.append(f'{app['flags'][i]}')
-                run_command.append(f'{user_config["apps"][app_name][i]}')
-            
-            for i in app['additional_args']:
-                if i in extra_global:
-                    run_command.append(f'{app['additional_args'][i]}')
-                    run_command.append(f'{user_config[extra_global[i]][i]}')
+            for flag, value in app_flags.items():
+                run_command.extend([value, user_config["apps"][app_name].get(flag, '')])
+
+            for arg, value in app_additional_args.items():
+                if arg in extra_global:
+                    run_command.extend([value, user_config[extra_global[arg]].get(arg, '')])
                 else:
-                    run_command.append(f'{app['additional_args'][i]}')
+                    run_command.extend([value])
 
             print(run_command)
-            subprocess.run(run_command,shell=True)
+            subprocess.run(run_command, shell=True)
             cls()
